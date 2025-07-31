1bc67f4fc0daf58ad31d54e5c17cc67143bac2a8
Rohith krishna <rohithkrishna04@outlook.com>
2025-07-31 19:28:15 +0530

    Update create.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import boto3
 # An EC2 client
 ec2 = boto3.client('ec2')
 
-# Runinng the ec2 machine
+# creating the ec2 machine
 Running = ec2.run_instances(
     ImageId= "ami-053b12d3152c0cc71",
     InstanceType= "t2.micro",
